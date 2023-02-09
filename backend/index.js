fb833fb8bf700adf7d79e85468e79419fa17794f
yug0231 <yugalnemade08@gmail.com>
2023-02-09 22:55:20 +0530

    userCreation backend done

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const cors = require('cors')
+const connect = require('./config/connect')
 require('dotenv').config();
 const port = process.env.port;
 const app = express();
+const userRoutes = require('./routes/userRoutes');
 app.use(cors());
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+app.use('/user', userRoutes);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-    console.log(port);
+connect()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}).catch((e)=>{
+    console.log("unexpected error occured", e);
 });
 
