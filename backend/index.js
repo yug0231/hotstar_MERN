ccb883f08b692e6dbbb1a2e0e126f6533ca6896f
yug0231 <yugalnemade08@gmail.com>
2023-02-09 21:37:34 +0530

    intialisation done

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+const port = process.env.port;
+const app = express();
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+    console.log(port);
+});
+
