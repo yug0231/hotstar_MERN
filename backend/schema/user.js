f88e24c16628f23c397733ad2449f1ef583babca
yug0231 <yugalnemade08@gmail.com>
2023-02-10 10:34:21 +0530

    allUserFUnctionalitydoneWishlistRemaining

--- backend/schema/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    name : String,
-    mobile : String
-})
-
-module.exports = mongoose.model("User", userSchema);
